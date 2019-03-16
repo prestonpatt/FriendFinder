5dc65fd9f807683365bb94dce7de6ce49e046e1c
Antifreeze <preston.patterson1@gmail.com>
2019-03-16 00:15:22 -0500

    fixing links

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
-var heroes = [
+var friends = [
     {
         "name": "Iron Man",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo": "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/e/e9/Iron_Man_AIW_Profile.jpg/revision/latest/scale-to-width-down/620?cb=20180518212029",
         "scores": [
             5,
             1,
@@ -17,84 +17,84 @@ var heroes = [
     },
     {
         "name": "The Hulk",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo": "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/c/c3/Hulk_AIW_Profile.jpg/revision/latest/scale-to-width-down/620?cb=20180518211829",
         "scores": [
             5,
+            4,
+            3,
             1,
-            4,
-            4,
             5,
             1,
-            2,
-            5,
-            4,
-            1
+            1,
+            1,
+            1,
+            2
         ]
     },
     {
         "name": "Black Widow",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo": "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/5/50/Black_Widow_AIW_Profile.jpg/revision/latest/scale-to-width-down/620?cb=20180518212205",
         "scores": [
             5,
-            1,
+            4,
             4,
             4,
             5,
-            1,
-            2,
-            5,
             4,
-            1
+            5,
+            3,
+            1,
+            4
         ]
     },
     {
         "name": "Thor",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo": "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/4/45/Thor_AIW_Profile.jpg/revision/latest/scale-to-width-down/620?cb=20180720191908",
         "scores": [
-            5,
             1,
-            4,
             4,
             5,
             1,
-            2,
-            5,
+            3,
             4,
-            1
+            5,
+            1,
+            5,
+            5
         ]
     },
     {
         "name": "Hawkeye",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo": "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/6/6f/CW_Textless_Shield_Poster_02.jpg/revision/latest/scale-to-width-down/620?cb=20180417151836",
         "scores": [
             5,
-            1,
             4,
-            4,
-            5,
-            1,
             2,
-            5,
             4,
-            1
+            1,
+            4,
+            5,
+            3,
+            1,
+            5
         ]
     },
     {
         "name": "Captain America",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo": "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/6/66/Captain_America_AIW_Profile.jpg/revision/latest/scale-to-width-down/620?cb=20180518211704",
         "scores": [
+            2,
             5,
-            1,
-            4,
-            4,
-            5,
+            3,
+            3,
             1,
             2,
             5,
             4,
-            1
+            2,
+            5
         ]
     }
 ]
 
-module.exports = heroes;
+module.exports = friends;
